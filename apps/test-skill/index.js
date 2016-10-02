0191bb0e612442f02275b022c135a7d4da133ea5
Paul <giansar26@gmail.com>
2016-10-02 03:32:45 -0700

    And so the fun begins

--- apps/test-skill/index.js
+++ apps/test-skill/index.js
@@ -20,10 +20,10 @@ app.error = function( exception, request, response ) {
 app.intent('askRecipe',
   {
 	"utterances":[ 
-		"What am I cooking?",
-    "What is for dinner?",
-    "What recipe is loaded?",
-    "What am I making?"]
+		"What am I cooking",
+    "What is for dinner",
+    "What recipe is loaded",
+    "What am I making"]
   },
   function(request,response) {
     response.say("The recipe I have prepared is " + food.recipe.name);
@@ -38,22 +38,22 @@ app.intent('askIngredients',
   },
   function(request,response) {
     var recipeList = "For this recipe you will need the following. ";
-    for (var i = 0; i <= food.amountIngredients - 1; i++) {
-      if (food.ingredients[i].quantity == "") {
-        recipeList += food.ingredients[i].name + "";
-      }
-      else if(food.ingredients[i].unit == ""){
-        recipeList += food.ingredients[i].quantity + " of " + food.ingredients[i].name;
-      }
-      else {
-        recipeList += food.ingredients[i].quantity + " " + food.ingredients[i].unit + " of " + food.ingredients[i].name;
-      }
-      if (food.ingredients[i].prep != "") {
-        recipeList += " " + food.ingredients[i].prep + ". ";
-      }
-      else {
-        recipeList += " . ";
-      }
+    // for (var i = 0; i <= food.amountIngredients - 1; i++) {
+    //   if (food.ingredients[i].quantity == "") {
+    //     recipeList += food.ingredients[i].name + "";
+    //   }
+    //   else if(food.ingredients[i].unit == ""){
+    //     recipeList += food.ingredients[i].quantity + " of " + food.ingredients[i].name;
+    //   }
+    //   else {
+    //     recipeList += food.ingredients[i].quantity + " " + food.ingredients[i].unit + " of " + food.ingredients[i].name;
+    //   }
+    //   if (food.ingredients[i].prep != "") {
+    //     recipeList += " " + food.ingredients[i].prep + ". ";
+    //   }
+    //   else {
+    //     recipeList += " . ";
+    //   }
     response.say(recipeList);
     }
   }
