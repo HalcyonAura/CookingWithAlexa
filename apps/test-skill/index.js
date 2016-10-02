b31f16e0427c35f3fa6f13b1a0edf8f9ddb95f1b
Paul <giansar26@gmail.com>
2016-10-02 03:18:00 -0700

    And so the fun begins

--- apps/test-skill/index.js
+++ apps/test-skill/index.js
@@ -29,4 +29,33 @@ app.intent('askRecipe',
     response.say("The recipe I have prepared is " + food.recipe.name);
   }
 );
+app.intent('askIngredients',
+  {
+  "utterances":[ 
+    "What do I need to start",
+    "What are the ingredients",
+    "What is the recipe list"]
+  },
+  function(request,response) {
+    var recipeList = "For this recipe you will need the following. ";
+    for (var i = food.ingredients.length - 1; i >= 0; i--) {
+      if (food.ingredients[i].quantity == "") {
+        recipeList += food.ingredients[i].name + "";
+      }
+      else if(food.ingredients[i].unit == ""){
+        recipeList += food.ingredients[i].quantity + " of " + food.ingredients[i].name;
+      }
+      else {
+        recipeList += food.ingredients[i].quantity + " " + food.ingredients[i].unit + " of " + food.ingredients[i].name;
+      }
+      if (food.ingredients[i].prep != "") {
+        recipeList += " " + food.ingredients[i].prep + ". "
+      }
+      else {
+        recipeList += " . "
+      }
+    response.say(recipeList);
+    }
+  }
+);
 module.exports = app;
